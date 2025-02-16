3c28154ba94a35f4b67bfd5888fc79934d37de77
Adriyansah Syaputra <adriansyahputra2626@gmail.com>
2025-02-16 20:33:07 +0700

    Setup project e-commerce with laravel 11 + react js and inertia

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <>
+        <h1 className="text-3xl font-bold text-center">Hello World</h1>
+        </>
+    )
+}
+
+export default Home;
